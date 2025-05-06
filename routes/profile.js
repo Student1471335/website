95af877a91d073ae2047f660a1c369e9d7601003
Matthew Lange <1471335@stu.ucenmanchester.ac.uk>
2025-05-06 01:52:49 +0100

    missed one and it now been cleared

--- routes/profile.js
+++ routes/profile.js
@@ -30,7 +30,7 @@ function readFromDB(UserID, res) {
   const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Spacewolf", //redact this in the final version
+    password: "Insert password here",//server root/adim password here 
     database: "Cityfinder"
   });
 
